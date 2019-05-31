3beafd0c62b7589ee22f7c0eea5d19da75303074
Donut <donut42915@gmail.com>
2019-05-31 15:39:54 -0400

    Add files via upload

---
+++ Scripts/Menu.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            SceneManager.LoadScene("Game", LoadSceneMode.Single);
+    }
+}
